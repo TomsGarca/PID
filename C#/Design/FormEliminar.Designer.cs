bd52afac37124dd408c8de7fa6f8a90be70e406f
Tomás García García <tomas.garciaga@alumno.buap.mx>
2022-02-04 15:37:01 -0600

    --

--- C#/Design/FormEliminar.Designer.cs
+++ C#/Design/FormEliminar.Designer.cs
@@ -17,19 +17,26 @@ namespace Interfaz
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
 
             this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "labe1";
             this.label1.Text = "Ingresa PID de proceso a eliminar:";
             this.label1.AutoSize = true;
 
+            this.textBox1.Location = new System.Drawing.Point(220, 20);
+            this.textBox1.Name = "TextBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 25);
+
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 250);
+            this.ClientSize = new System.Drawing.Size(300, 250);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "EliminarTarea";
             this.Text = "Eliminar Tarea";
         }
         
         private Label label1;
+        private TextBox textBox1;
     }
 }
